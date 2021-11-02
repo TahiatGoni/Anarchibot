8eb68df25d2067734a5603cd30827e6c79f65412
Tahiat Goni <46511001+TahiatGoni@users.noreply.github.com>
2021-11-02 00:57:28 -0600

    Small changes made.

--- anarchibot.cpp
+++ anarchibot.cpp
@@ -1,5 +1,10 @@
 #include "anarchibot.h"
+#include <iostream>
 
-void anarchibot::OnGameStart() { return; }
+void anarchibot::OnGameStart() { 
+	std::cout << "GO Anarchibot" << std::endl; 
+}
 
-void anarchibot::OnStep() { return; }
+void anarchibot::OnStep() { 
+	return; 
+}
